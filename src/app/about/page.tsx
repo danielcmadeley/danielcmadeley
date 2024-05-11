4cbee7553f4517c16b4d1191ccc4575500308507
Dan <dev@madeleydesignstudio.org>
2024-05-11 18:44:13 +0100

    added github

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import GitHubCalendar from "react-github-calendar";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <h2 className="text-2xl ">git add -A</h2>
+        <h2 className="text-2xl ">git commit -m &quot;add&quot;</h2>
+        <h2 className="text-2xl ">git push</h2>
+        <GitHubCalendar username="mxdeley" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
