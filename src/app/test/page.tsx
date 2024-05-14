11bb8affa244797ed8fb599be5059c46f6cb9352
mxdeley <dev@madeleydesignstudio.org>
2024-05-14 23:14:53 +0100

    added initial 3d model

---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Three from "./_components/Three";
+
+const Test = () => {
+  return (
+    <div className="w-screen h-screen">
+      <Three />
+    </div>
+  );
+};
+
+export default Test;
