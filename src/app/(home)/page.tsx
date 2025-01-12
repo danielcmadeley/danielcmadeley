58619ca7d15237db2bd7f9c5f5f6d941c29e9c4b
Dan <dev@madeleydesignstudio.org>
2025-01-12 17:08:37 +0000

    added refresh ability

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,12 +2,11 @@ import { Suspense } from 'react'
 import { DownloadCV } from '../components/download-cv'
 import Sidebar from '../components/sidebar'
 
-export default function Home({ children }: { children: React.ReactNode }) {
+export default function Home() {
   return (
     <main className="h-full w-full flex flex-col overflow-hidden">
       <div className="h-full grid grid-cols-3">
         <Sidebar />
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
       </div>
       <DownloadCV />
     </main>
